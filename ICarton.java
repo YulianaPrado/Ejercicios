4a944a7f07f455eb577d9011e3c6b89cb67bc71d
YulianaPrado <u20242229090@usco.edu.co>
2025-08-11 07:57:31 -0500

    Delete ICarton.java

--- ICarton.java
+++
@@ -1,8 +0,0 @@
-package JuegoDelBingo;
-
-public interface ICarton {
-    public void rellenarCarton();
-    public boolean comprobarNumero(int numero);
-    public boolean estaCompleto();
-    public void mostrarCarton();
-}///YulianaPrado
